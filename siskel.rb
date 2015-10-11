db84876e18ec0ed00965cf3a3a549e7f2773f22d
Daniel Brito <dbrito2381@hotmail.com>
2015-10-11 15:59:46 -0400

    New Siskel Files

--- siskel.rb
+++ siskel.rb
@@ -1,10 +1,36 @@
 require 'httparty'
 
 class Siskel
-  attr_reader :title
+	attr_reader :rating
+	attr_reader :year
+	attr_reader :plot
 
-  def initialize(title)
-    @movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}")
-    @title = @movie['Title']
-  end
-end
+	def initialize(title, opts = {} )
+		@movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}&y=#{opts[:year]}&plot=#{opts[:plot]}&tomatoes=true")
+		@title = @movie['Title']
+		@rating = @movie['Rated']
+		@year = @movie['Year']
+		@plot = @movie['Plot']
+	end
+
+	def title
+		if (@title == nil) 
+			@title = @movie['Error']
+		else
+		(@title)
+	end
+	end
+
+		def consensus
+			rating = @movie['tomatoMeter'].to_i
+			if ( rating < 25 )
+				"2 Thumbs Down"
+			elsif (rating <50)
+				"Thumbs Down"
+			elsif (rating < 75)
+				"Thumbs Up"
+			else
+				"2 Thumbs Up"
+			end
+		end
+ end
